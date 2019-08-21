f973c40dfdae5217147934038d68cb50e542c7e7
Steven Garis <stevengaris@gmail.com>
2019-08-21 18:31:14 -0500

    helper is in, both functions working and good to go.

--- searching/linear_search/linear_search.py
+++ searching/linear_search/linear_search.py
@@ -1,18 +1,19 @@
 
+# helper function to build list
 def ordered_sequential_list(min_num, max_num):
     return list(range(min_num, max_num+1))
 
-
+# create list with min and max of your choice
 List = ordered_sequential_list(0,100)
 
-
+# give list and the number you want to search for
 def linear_search(List, number):
     if number not in List:
         return -1
     else:
         return List.index(number)
 
-
+# testing function
 print(linear_search(List, 2))
 print(linear_search(List, 97))
 print(linear_search(List, 101))
